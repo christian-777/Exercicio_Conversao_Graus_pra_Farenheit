c4001ba6f31048d54408fdb6a0303ff5ea020b5b
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-05 15:21:30 -0300

    Add project files.

---
+++ Exercicio_Conversao_Graus_pra_Farenheit/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //exercicio de conversao de celcius para farenheit
+
+        double temp, converted;
+
+        Console.WriteLine("Digite a temperatura em celcius: ");
+        temp = double.Parse(Console.ReadLine());
+
+        converted = (temp * 1.8) + 32;
+
+        Console.WriteLine("celcius: " + temp + "\n" + "farenheit: " + converted);
+
+    }
+}
